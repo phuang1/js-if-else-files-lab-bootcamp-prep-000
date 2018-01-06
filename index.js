f148cdfea54b7c60d7396eaeaad044afc3487b9f
p huang <huang.pien@gmail.com>
2018-01-06 05:09:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var height = "74"
 
 if (name === "Susan"){
 console.log ("The name is correct.")
-}
+} else {
+    console.log("Expected her name to be Susan.")}
 
 // Don't worry about this
 module.exports = { name, height
